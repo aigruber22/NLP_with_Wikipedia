b9fb240d04ff7844593b13ee5e32f9d041030d87
Adam Gruber <adam.i.gruber@gmail.com>
2018-01-08 22:52:33 -0500

    Initial commit from old repo

---
+++ ipynb/__init__.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import pymongo
+import re
+import requests
+from bs4 import BeautifulSoup
---
+++ ipynb/nlp.py
@@ -0,0 +1,11 @@
+import spacy
+import nltk
+
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import LabelEncoder
